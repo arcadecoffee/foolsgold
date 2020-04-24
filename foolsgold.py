03d1b542a64b0dd2bcacf935ae230175580efc62
arcadecoffee <12764780+arcadecoffee@users.noreply.github.com>
2020-04-24 15:10:03 -0500

    Update foolsgold.py

--- foolsgold.py
+++ foolsgold.py
@@ -11,11 +11,13 @@ rss_template = os.path.join('templates', 'jeff_ward_show.rss.jinja2')
 rss_output = os.path.join('rss', 'jeff_ward_show.rss')
 episode_length = timedelta(hours=3)
 max_episode_age = timedelta(days=7)
+file_mode = 0o644
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 recording_filename = os.path.join(media_directory, f'{datetime.now().strftime("%Y%m%d")}.mp3')
 jws.record_show(dest_filename=recording_filename, duration=episode_length)
+os.chmod(dest_filename, file_mode)
 
 podcasts = PodcastCollection(media_directory)
 podcasts.purge_old_episodes(max_episode_age)
